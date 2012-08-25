2eeb82820c72e430b3885e57737b070bbaba1346
Richard Z.H. Wang <zhwang@live.com.au>
2012-08-25 10:22:52 +1000

    Add Sublime Text 2
    So far this stuff is mostly Windows-specific.
    
    Ignores include Base File as that seems more OS-specific
    (e.g. on my computer it specifies Consolas as the font)

--- install-windows.cmd
+++ install-windows.cmd
@@ -1,10 +1,16 @@
 @echo OFF
 
 IF %ERRORLEVEL% EQU 0 (
-	REM We need to use a hard link otherwise Vim doesn't pick it up
+	:: ======= SUBLIME TEXT 2 =======
+	rmdir /S /Q "%APPDATA%\Sublime Text 2\Packages\User"
+	mklink /J "%APPDATA%\Sublime Text 2\Packages\User" "%CD%\sublime2\User"
+
+	:: ======= VIM =======
+	del "%USERPROFILE%\.vimrc"
+	:: We need to use a hard link otherwise Vim doesn't pick it up
 	mklink /H "%USERPROFILE%\.vimrc" "%CD%\vim\vimrc"
-	PAUSE
 ) ELSE (
 	echo This script needs to be run as administrator!
-	PAUSE
 )
+
+PAUSE
