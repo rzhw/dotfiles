e7ac7915625ebc2ebbdea2172c67178269ece4d3
Richard Wang <zhwang@live.com.au>
2015-10-05 11:50:21 +1100

    install-unix: Initialise submodules first

--- install-unix.sh
+++ install-unix.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# ======= SUBMODULES =======
+echo "* Submodules"
+git submodule init
+git submodule update
+
 # ======= VIM =======
+echo "* Vim"
 ln -sf "`pwd`/vim/vimrc" "$HOME/.vimrc"
 ln -sf "`pwd`/vim/vim" "$HOME/.vim"
